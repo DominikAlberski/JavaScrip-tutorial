cde7ba73aff0c1ccb656552e32ec5198df21becb
Dominik <dominik.alberski@gmail.com>
2019-03-29 08:38:57 +0100

    Palindrome detector first version

---
+++ bundle.js
@@ -0,0 +1,50 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+let Phrase = require("dal-palindrome-js");
+
+let string = prompt("Please enter a string for palindrome testing:")
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()) {
+  alert(`"${phrase.content}" is a palindrome!`);
+} else {
+  alert(`"${phrase.content}" is not a palindrome!`);
+}
+
+alert(new Phrase("racecar").palindrome());
+
+},{"dal-palindrome-js":2}],2:[function(require,module,exports){
+module.exports = Phrase;
+
+// Reverses a string.
+
+// Returns true for a palindrome, false otherwise.
+function palindrome(string) {
+  let processedContent = string.toLowerCase();
+  return processedContent === reverse(processedContent);
+}
+
+function reverse(string) {
+  return Array.from(string).reverse().join("")
+}
+
+
+
+function Phrase(content) {
+  this.content = content;
+
+  this.palindrome = function palindrome() {
+    let processedContent = this.letters().toLowerCase();
+    return processedContent === reverse(processedContent);
+  }
+
+  this.letters = function letters() {
+    const letterR = /[a-z]/ig;
+    return (this.content.match(letterR) || []).join("");
+  }
+
+  this.louder = function louder(){
+    return this.content.toUpperCase();
+  }
+}
+
+},{}]},{},[1]);
