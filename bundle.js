694d73ca16ee5bf2e74796a41ba18d00e2ebe098
Dominik <dominik.alberski@gmail.com>
2019-04-03 09:00:54 +0200

    Display result as dynamic HTML

--- bundle.js
+++ bundle.js
@@ -1,16 +1,26 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let Phrase = require("dal-palindrome-js");
 
-let string = prompt("Please enter a string for palindrome testing:")
-let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome!`);
+function palindromeTerster() {
+  let string = prompt("Please enter a string for palindrome testing:")
+  let phrase = new Phrase(string);
+
+  let palindromeResult = document.querySelector('[data-js-result]')
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = (`"${phrase.content}" is a palindrome!`);
+  } else {
+    palindromeResult.innerHTML = (`"${phrase.content}" is not a palindrome!`);
+  }
 }
 
-alert(new Phrase("racecar").palindrome());
+document.addEventListener("DOMContentLoaded", function () {
+  let button = document.querySelector('[data-js-button-palindrome]');
+  button.addEventListener("click", function() {
+    palindromeTerster();
+  });
+})
 
 },{"dal-palindrome-js":2}],2:[function(require,module,exports){
 module.exports = Phrase;
--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 let Phrase = require("dal-palindrome-js");
 
-let string = prompt("Please enter a string for palindrome testing:")
-let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome!`);
+function palindromeTerster() {
+  let string = prompt("Please enter a string for palindrome testing:")
+  let phrase = new Phrase(string);
+
+// when using data attribute as querySelector make shure that in html it has proer value
+  let palindromeResult = document.querySelector('[data-js-result]')
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = (`"${phrase.content}" is a palindrome!`);
+  } else {
+    palindromeResult.innerHTML = (`"${phrase.content}" is not a palindrome!`);
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  let button = document.querySelector('[data-js-button-palindrome]');
+  button.addEventListener("click", function() {
+    palindromeTerster();
+  });
+})
